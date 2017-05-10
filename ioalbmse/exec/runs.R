88c907e92732f6da9cfdd642d7a129a7797f5bdd
Iago Mosqueira <iago@mosqueira.eu>
2017-05-10 15:55:32 +0200

    SR residuals from history

--- ioalbmse/exec/runs.R
+++ ioalbmse/exec/runs.R
@@ -14,50 +14,81 @@ registerDoParallel(5)
 # --- SETUP
 
 # data
-
 data(indicators)
 data(oms)
 
+# CPUE
 cpue <- propagate(window(ocpue$index, end=2040), 200)
 cpuesel <- ocpue$sel.pattern[-1,'2014']
 
-# options
+# SR residuals: sd, b
+resd <- c(sqrt(yearVars(ocpue$index.res))*4)
+rerho <- c(apply(ocpue$index.res, c(1,3:6), function(x) acf(x, plot=FALSE)$acf[2]))
+sres <- FLQuant(0, dimnames=dimnames(stock(omp)))
+for(i in seq(dim(sres)[6]))
+  sres[,,,,,i] <- rlnoise(1, sres[,,,,,i], sd=resd[1], b=0)
+osr@.Data[['residuals']] <- sres
 
+# ARGUMENTS
 years <- seq(2014, 2034, by=2)
 
 
 # --- LONG TERM projection at F_bar~0
 
-ltom <- fwd(omp, sr=osr,
+Rltom <- fwd(omp, sr=osr, residuals=osr$residuals,
   control=fwdControl(quant='f', year=2015:2040, value=0.0001))
+
+# --- CONSTANT catch
+
+Rconc <- fwd(omp, sr=osr, residuals=osr$residuals,
+  control=fwdControl(quant='catch', year=2015:2040, value=catch(omp)[,'2014']))
+
+# --- CONSTANT F
+
+Rconf <- fwd(omp, sr=osr, residuals=osr$residuals,
+  control=fwdControl(quant='f', year=2015:2040, value=fbar(omp)[,'2014']))
+
+plot(Rltom, Rconc, Rconf)
+
 
 # --- msePT
 
 # Robustness trial
 
-R0b <- msePT(omp, osr, cpue=cpue, cpuesel=cpuesel, years=years,
-  hcrparams=FLPar(lambda=1, dltac=0.15, dhtac=0.15, Dlimit=0.10, Dtarget=0.40), 
-  oemparams=FLPar(b=0.2, sd=0.3))
+R0pt <- fwd(omp, sr=osr,
+  control=fwdControl(list(year=2014, quant="f", value=fbar(omp)[,'2014'] * 5)))
 
-plot(R0b$om) + geom_vline(aes(xintercept=an(ISOdate(2015,1,1))))
+R0pt <- msePT(R0, osr, cpue=cpue, cpuesel=cpuesel, years=2015:2034,
+  hcrparams=FLPar(lambda=1, dltac=0.15, dhtac=0.15, Dlimit=0.10, Dtarget=0.40), 
+  oemparams=FLPar(sd=0.3, b=0.2))
+
+plot(R0pt$om) + geom_vline(aes(xintercept=an(ISOdate(2015,1,1))))
+
 
 # Tuning
 
-ptgrid <- list(lambda=seq(0.50, 1.50, length=3),
+grid <- list(lambda=seq(0.50, 1.50, length=5),
   dltac=seq(0.05, 0.25, length=2), dhtac=seq(0.05, 0.25, length=2),
   Dlimit=seq(0.10, 0.20, length=2), Dtarget=seq(0.30, 0.50, length=2))
 
-system.time(RG <- doRuns(msePT, grid=ptgrid, omp=omp, sr=osr, dlag=2,
+# TEST
+grid <- list(lambda=seq(0.50, 1.50, length=5),
+  dltac=0.10, dhtac=0.15, Dlimit=c(0.10, 0.20), Dtarget=c(0.40, 0.50))
+
+system.time(RG <- doRuns(msePT, grid=grid, omp=omp, sr=osr, dlag=2,
   cpue=cpue, cpuesel=cpuesel, years=years, oemparams=FLPar(b=0, sd=0)))
 
-plotOMR(om, RG)
+plotOMR(om, RG, qname='fbar')
+plotOMR(om, RG, qname='catch')
 
 # P(SB_2038 > SBMSY)
 t1 <- lapply(RG, function(x) sum(c(ssb(x)[,ac(2015:2034)]) > c(rpts$SBMSY)) / (200 * 24))
 
-pRG <- performance(RG, indicators, rpts, years=2034, grid=ptgrid)
+pRG <- performance(RG, indicators, rpts, years=2034, grid=grid)
 qRG <- performance(RG, indicators, rpts, years=2034,
-  probs=c(0.1, 0.25, 0.50, 0.75, 0.90), grid=ptgrid)
+  probs=c(0.1, 0.25, 0.50, 0.75, 0.90), grid=grid)
+
+qRG2 <- quantilePerf(pRG)
 
 qRG[indicator == 'S8', `50%`>0.40 & `50%`<0.60]
 
@@ -79,7 +110,6 @@ save(RG, pRG, qRG, file="msePT_grid.RData", compress="xz")
 R0 <- mseIndex(omp=omp, sr=osr, cpue=index,
   hcrparams=FLPar(lambda=0.95, ny=5, dltac=0.20, dhtac=0.10),
   years=years, oemparams=FLPar(b=0, sd=0), imparams=NA, verbose=TRUE)
-
 plot(R0$om) + geom_vline(aes(xintercept=as.numeric(ISOdate(years[1],1,1))))
 
 ggplot(ssb(R0$om), aes(x=date, y=data, colour=iter)) + geom_line() + geom_vline(aes(xintercept=as.numeric(ISOdate(2014,1,1)))) + theme(legend.position='none')
